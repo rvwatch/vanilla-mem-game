2ce472792648fec57dc6c2e4a1849ae667622fef
rvwatch <astro.viera@gmail.com>
2021-09-28 13:32:08 -0600

    adds full funcitonality for game

--- scripts.js
+++ scripts.js
@@ -1,71 +1,93 @@
 const startGame = () => {
   const board = document.getElementsByTagName('section')[0];
 
-  // I want to create 2 buttons per num in an array of 16 random nums
-  // They need to be rendered to the page in random locations
-  // the two cards need to be associated somehow or their vals equated when selected
-  // create a random array of 16 nums between 0-100
-  // loop over the array
-  //    loop over every num in array twice
-  //       create button element with num val
-  //       add class to button
-  //       append button to section -- this needs to be randomized
-  //           add this html to an array? randomize index placement?
-  //        
-  //           
-
-
   const populateBoard = () => {
     let cardList = [];
     for (let i = 0; i < 16; i++) {
+      //TODO: Need to filter for more than 2 duplicates
+      // get random number if 2 instances of the rando number already exist, re-run randomNumber before moving on 
       let rando = randomNum(1, 100);
+
       for (let j = 0; j < 2; j++) {
         let card = document.createElement('article');
         let cardFront = document.createElement('button');
         let cardBack = document.createElement('button');
-        card.setAttribute('class', `card`);
-        cardFront.setAttribute('class', `front`);
-        cardBack.setAttribute('class', `back`);
+
+        card.classList.add('card');
+        cardFront.classList.add('front');
+        cardBack.classList.add('back');
         cardBack.innerText = rando;
 
         card.appendChild(cardFront);
         card.appendChild(cardBack);
-        
+
         cardList.push(card);
       }
     }
-    
+
     for (let i = 0; i < 32; i++) {
-      
       const rando = randomNum(0, cardList.length - 1);
-      let randomCard = cardList.splice(rando, 1);
-      board.appendChild(randomCard[0]);
+      let randomCard = cardList.splice(rando, 1)[0];
+      board.appendChild(randomCard);
+      console.log(randomCard.innerText);
+    }
+
+    let flippable = true;
+
+    const manageCards = event => {
+      if (flippable) {
+        event.target.parentElement.classList.add('is-flipped');
+      }
+
+      let selected = document.querySelectorAll('.is-flipped');
+      if (selected.length === 2) {
+        flippable = false;
+        checkForMatch(selected);
+      }
+    }
+
+    const checkForMatch = (selected) => {
+      let firstCard = selected[0];
+      let secondCard = selected[1];
+
+      if (firstCard.innerText === secondCard.innerText) {
+        firstCard.classList.add('matched');
+        secondCard.classList.add('matched');
+        firstCard.classList.remove('is-flipped');
+        secondCard.classList.remove('is-flipped');
+        firstCard.removeEventListener('click', manageCards);
+        secondCard.removeEventListener('click', manageCards);
+        flippable = true;
+        checkForWin()
+      } else {
+        setTimeout(() => {
+          firstCard.classList.remove('is-flipped');
+          secondCard.classList.remove('is-flipped');
+          flippable = true;
+        }, 1000);
+      }
+    }
+
+    const checkForWin = () => {
+      let allMatched = document.querySelectorAll('.matched');
+
+      if (allMatched.length === 32) {
+        setTimeout(() => {
+          alert('YOU WIN!!');
+        }, 1000)
+
+      }
     }
 
     let cards = document.querySelectorAll('.card');
-    
-    const manageCards = event => {
-      console.log(event.target.classList)
-    }
-
     cards.forEach(card => card.addEventListener('click', manageCards));
-
   }
 
-  const randomNum = (min, max) => { 
+  const randomNum = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-
-
   populateBoard();
 }
-
-
-
-
-
-
-
 
 window.addEventListener('load', startGame)
